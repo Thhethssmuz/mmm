cb2681696a64d185860a67126cc2b01b78fc7830
Thhethssmuz <thhethssmuz@gmail.com>
2014-06-08 22:57:24 +0200

    changed from typedefs to using c++11 type alias

--- src/mmm.hpp
+++ src/mmm.hpp
@@ -8,57 +8,13 @@
 #include <cmath>
 #include <iostream>
 
-/*
-  TODO:
-    be consistent about using upper case for template vars
-      change all mentions of size_t n to size_t N
-      also Elems, and L,
-  --------------------------------------------------------
-
-  typefu/
-    traits
-    restrict
-    promote
-    components
-    gen
-
-  mat/
-
-  swizzle/
-    swizzle
-    swizzle1
-
-    opr/
-      asig
-      enum
-
-  vec/           mostly complete
-    vecType      no explicit tests
-    tvec         complete!
-    tvec2        no explicit tests
-
-    opr/         complete!
-      asig       complete!
-      enum       complete!
-      num        complete!
-      comp       complete!
-      io         complete!
-
-    func/        lacking some functions
-      trig       failed test 'atan2'!
-      exp        complete!
-      common     2 missing: smoothstep
-      geo        3 missing: faceforward, reflect, refract
-      rel        complete!
-      reduct     complete!
-      io         complete!
-*/
 
 #include <typefu/traits.hpp>
 #include <typefu/restrict.hpp>
 #include <typefu/promote.hpp>
 #include <typefu/components.hpp>
 #include <typefu/gen.hpp>
+
 
 #include <vec/vecType.hpp>
 #include <swizzle/swizzle.hpp>
@@ -79,6 +35,7 @@
 #include <vec/func/rel.hpp>
 #include <vec/func/reduct.hpp>
 #include <vec/func/io.hpp>
+
 
 #include <vec/vecType.tpp>
 #include <swizzle/swizzle.tpp>
@@ -101,30 +58,34 @@
 #include <vec/func/io.tpp>
 
 
-template <size_t n> using  vec = tvec<float,n>;
-template <size_t n> using dvec = tvec<double,n>;
-template <size_t n> using bvec = tvec<bool,n>;
-template <size_t n> using ivec = tvec<int,n>;
-template <size_t n> using uvec = tvec<unsigned int,n>;
+template <size_t n>
+using vec  = tvec<float,n>;
+using vec2 = vec<2>;
+using vec3 = vec<3>;
+using vec4 = vec<4>;
 
-typedef vec<2> vec2;
-typedef vec<3> vec3;
-typedef vec<4> vec4;
+template <size_t n>
+using dvec  = tvec<double,n>;
+using dvec2 = dvec<2>;
+using dvec3 = dvec<3>;
+using dvec4 = dvec<4>;
 
-typedef dvec<2> dvec2;
-typedef dvec<3> dvec3;
-typedef dvec<4> dvec4;
+template <size_t n>
+using bvec  = tvec<bool,n>;
+using bvec2 = bvec<2>;
+using bvec3 = bvec<3>;
+using bvec4 = bvec<4>;
 
-typedef bvec<2> bvec2;
-typedef bvec<3> bvec3;
-typedef bvec<4> bvec4;
+template <size_t n>
+using ivec  = tvec<int,n>;
+using ivec2 = ivec<2>;
+using ivec3 = ivec<3>;
+using ivec4 = ivec<4>;
 
-typedef ivec<2> ivec2;
-typedef ivec<3> ivec3;
-typedef ivec<4> ivec4;
-
-typedef uvec<2> uvec2;
-typedef uvec<3> uvec3;
-typedef uvec<4> uvec4;
+template <size_t n>
+using uvec  = tvec<unsigned int,n>;
+using uvec2 = uvec<2>;
+using uvec3 = uvec<3>;
+using uvec4 = uvec<4>;
 
 #endif
