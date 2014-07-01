f969523efe0831b768bdfab78eb009013ec90ae6
Thhethssmuz <thhethssmuz@gmail.com>
2014-07-02 01:25:20 +0200

    fixed overlooked old template naming convention

--- src/vec/opr/io.hpp
+++ src/vec/opr/io.hpp
@@ -1,10 +1,10 @@
 #ifndef mmm_vec_opr_io_hpp
 #define mmm_vec_opr_io_hpp
 
-template <typename T, size_t n, typename A>
-std::ostream& operator<<(std::ostream& os, const vecType<T, n, A>& v);
+template <typename T, size_t N, typename A>
+std::ostream& operator<<(std::ostream& os, const vecType<T, N, A>& v);
 
-template <typename T, size_t n>
-std::ostream& operator<<(std::ostream& os, const tvec<T, n>& v);
+template <typename T, size_t N>
+std::ostream& operator<<(std::ostream& os, const tvec<T, N>& v);
 
 #endif
