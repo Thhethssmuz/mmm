ad3b96c6cfef95c212e3193f9d10055ba3124dae
Thhethssmuz <thhethssmuz@gmail.com>
2017-01-15 11:27:37 +0100

    why would I want this

--- src/mat/func/io.hpp
+++ src/mat/func/io.hpp
@@ -1,5 +1,5 @@
 #pragma once
 
-// prints matrix to stdout in row-major order
+// prints matrix to stdout in column-major order
 template <typename T, size_t N, size_t M>
 void print(const tmat<T, N, M>& m);
