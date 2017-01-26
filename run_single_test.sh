a50297350c5d9b6afde110e3e290a0ae31fd423e
Thhethssmuz <thhethssmuz@gmail.com>
2017-01-26 21:54:36 +0100

    since tests are not built by default

--- run_single_test.sh
+++ run_single_test.sh
@@ -5,6 +5,6 @@ name=$(echo "$1" | sed "s/test\///" | sed "s/\//_/g" | sed "s/.cpp//")
 name="mmm_${name}_test"
 mkdir -p build
 cd build
-cmake .. -DUSE_LIBDW=0
+cmake .. -DMMM_BUILD_UNIT_TESTS=1 -DUSE_LIBDW=0
 make -j4 $name #VERBOSE=1
 ctest -R $name --output-on-failure
--- run_tests.sh
+++ run_tests.sh
@@ -3,6 +3,6 @@ set -e
 
 mkdir -p build
 cd build
-cmake .. -DUSE_LIBDW=0
+cmake .. -DMMM_BUILD_UNIT_TESTS=1 -DUSE_LIBDW=0
 make -j4 mmm_tests #VERBOSE=1
 ctest -R mmm_tests --output-on-failure
