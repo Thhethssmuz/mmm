9718673be93b954658e5eb4a62ae15b91661ae71
Thhethssmuz <thhethssmuz@gmail.com>
2017-02-05 15:59:35 +0100

    update affine matrix functions to column major

--- test/mat/func/affine.cpp
+++ test/mat/func/affine.cpp
@@ -4,30 +4,42 @@
 namespace {
   using namespace mmm;
 
-  auto ami = UnitTest("affine matrices - identity", +[] {
+  auto am_id1 = UnitTest("affine matrices - identity (1)", +[] {
 
-    if (mat2::identity != mat2(1,0, 0,1)) return false;
-    if (mat3::identity != mat3(1,0,0, 0,1,0, 0,0,1)) return false;
-    if (mat4::identity != mat4(1,0,0,0, 0,1,0,0, 0,0,1,0, 0,0,0,1)) return false;
+    if (identity<float, 2, 2>() != mat2x2(1, 0, 0, 1)) return false;
+    if (identity<float, 2, 3>() != mat2x3(1, 0, 0, 1, 0, 0)) return false;
+    if (identity<float, 2, 4>() != mat2x4(1, 0, 0, 1, 0, 0, 0, 0)) return false;
 
-    if (identity<float, 2>() != mat2(1,0, 0,1)) return false;
-    if (identity<float, 3>() != mat3(1,0,0, 0,1,0, 0,0,1)) return false;
-    if (identity<float, 4>() != mat4(1,0,0,0, 0,1,0,0, 0,0,1,0, 0,0,0,1)) return false;
+    if (identity<float, 3, 2>() != mat3x2(1, 0, 0, 0, 1, 0)) return false;
+    if (identity<float, 3, 3>() != mat3x3(1, 0, 0, 0, 1, 0, 0, 0, 1)) return false;
+    if (identity<float, 3, 4>() != mat3x4(1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0)) return false;
+
+    if (identity<float, 4, 2>() != mat4x2(1, 0, 0, 0, 0, 1, 0, 0)) return false;
+    if (identity<float, 4, 3>() != mat4x3(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0)) return false;
+    if (identity<float, 4, 4>() != mat4x4(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1)) return false;
+
+    return true;
+  });
+  auto am_id2 = UnitTest("affine matrices - identity (2)", +[] {
+
+    if (mat2::identity != mat2(1, 0, 0, 1)) return false;
+    if (mat3::identity != mat3(1, 0, 0, 0, 1, 0, 0, 0, 1)) return false;
+    if (mat4::identity != mat4(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1)) return false;
 
     return true;
   });
 
-  auto amt = UnitTest("affine matrices - translate", +[] {
+  auto am_trans1 = UnitTest("affine matrices - translate (1)", +[] {
     mat4 t1 = translate(1.f, 2.f, 3.f);
     mat4 t2 = translate(vec3(3, 2, 1));
 
-    if (t1 != mat4(1,0,0,1, 0,1,0,2, 0,0,1,3, 0,0,0,1)) return false;
-    if (t2 != mat4(1,0,0,3, 0,1,0,2, 0,0,1,1, 0,0,0,1)) return false;
+    if (t1 != mat4(1, 0, 0, 1, 0, 1, 0, 2, 0, 0, 1, 3, 0, 0, 0, 1)) return false;
+    if (t2 != mat4(1, 0, 0, 3, 0, 1, 0, 2, 0, 0, 1, 1, 0, 0, 0, 1)) return false;
 
     return true;
   });
 
-  auto amr = UnitTest("affine matrices - rotate", +[] {
+  auto am_rot1 = UnitTest("affine matrices - rotate (1)", +[] {
     mat4 rx = rotate_x(45.f);
     mat4 ry = rotate_y(45.f);
     mat4 rz = rotate_z(45.f);
@@ -50,7 +62,7 @@ namespace {
     return true;
   });
 
-  auto ams = UnitTest("affine matrices - scale", +[] {
+  auto am_scale1 = UnitTest("affine matrices - scale (1)", +[] {
     mat4 s = scale(2.f, 3.f, 4.f);
 
     if (s != mat4(2,0,0,0, 0,3,0,0, 0,0,4,0, 0,0,0,1)) return false;
