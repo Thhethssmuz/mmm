097c9d8e5dd14dc8cb3b8681b609d110dd7385c8
Thhethssmuz <thhethssmuz@gmail.com>
2017-02-07 21:12:11 +0100

    update swizzle tests to catch

--- test/swizzle/opr/asig.cpp
+++ test/swizzle/opr/asig.cpp
@@ -1,278 +1,239 @@
-#include <unittest.hpp>
+#include <catch.hpp>
 #include <mmm.hpp>
 
-namespace {
-  using namespace mmm;
-
-  auto swiz_add1 = UnitTest("swizzle assignment operator += (1)", +[]() {
-    ivec4 v = ivec4(0, 1, 2, 3);
-
-    v.x += 1;
-    v.xy += 1;
-    v.zyx += 1;
-    v.xwzy += 1;
-
-    if (v != 4) return false;
-
-    return true;
-  });
-  auto swiz_add2 = UnitTest("swizzle assignment operator += (2)", +[]() {
-    ivec4 v = ivec4(4);
-
-    v.xy += ivec2(2, 1);
-    v.yzw += ivec3(1, 2, 3);
-    v.xyzw += ivec4(-1, 0, 1, 1);
-
-    if (v != ivec4(5, 6, 7, 8)) return false;
-
-    return true;
-  });
-  auto swiz_add3 = UnitTest("swizzle assignment operator += (3)", +[]() {
-    ivec4 v = ivec4(5, 6, 7, 8);
-
-    v.x += 1;
-    v.yy += 1;
-    v.zzz += 1;
-    v.wwww += 1;
-
-    if (v != ivec4(6, 8, 10, 12)) return false;
-
-    return true;
-  });
-  auto swiz_add4 = UnitTest("swizzle assignment operator += (4)", +[]() {
-    ivec4 v = ivec4(6, 8, 10, 12);
-
-    v.x += v.x;
-    v.yx += v.xy;
-    v.zyx += v.xyz;
-    v.wzyx += v.xyzw;
-
-    if (v != ivec4(42, 70, 70, 42)) return false;
-
-    return true;
-  });
-
-  auto swiz_sub1 = UnitTest("swizzle assignment operator -= (1)", +[]() {
-    ivec4 v = ivec4(3, 2, 1, 0);
-
-    v.x -= 1;
-    v.xy -= 1;
-    v.zyx -= 1;
-    v.xwzy -= 1;
-
-    if (v != -1) return false;
-
-    return true;
-  });
-  auto swiz_sub2 = UnitTest("swizzle assignment operator -= (2)", +[]() {
-    ivec4 v = ivec4(-1);
-
-    v.xy -= ivec2(2, 1);
-    v.yzw -= ivec3(1, 2, 3);
-    v.xyzw -= ivec4(-1, 0, 1, 1);
-
-    if (v != ivec4(-2, -3, -4, -5)) return false;
-
-    return true;
-  });
-  auto swiz_sub3 = UnitTest("swizzle assignment operator -= (3)", +[]() {
-    ivec4 v = ivec4(-2, -3, -4, -5);
-
-    v.x -= 1;
-    v.yy -= 1;
-    v.zzz -= 1;
-    v.wwww -= 1;
-
-    if (v != ivec4(-3, -5, -7, -9)) return false;
-
-    return true;
-  });
-  auto swiz_sub4 = UnitTest("swizzle assignment operator -= (4)", +[]() {
-    ivec4 v = ivec4(-3, -5, -7, -9);
-
-    v.x -= v.x;
-    v.yx -= v.xy;
-    v.zyx -= v.xyz;
-    v.wzyx -= v.xyzw;
-
-    if (v != ivec4(21, 12, -12, -21)) return false;
-
-    return true;
-  });
-
-  auto swiz_mult1 = UnitTest("swizzle assignment operator *= (1)", +[]() {
-    ivec4 v = ivec4(1, 2, 4, 8);
-
-    v.x *= 2;
-    v.xy *= 2;
-    v.zyx *= 2;
-    v.xwzy *= 2;
-
-    if (v != 16) return false;
-
-    return true;
-  });
-  auto swiz_mult2 = UnitTest("swizzle assignment operator *= (2)", +[]() {
-    ivec4 v = ivec4(16);
-
-    v.xy *= ivec2(4, 2);
-    v.yzw *= ivec3(2, 4, 8);
-    v.xyzw *= ivec4(1, 2, 4, 4);
-
-    if (v != ivec4(64, 128, 256, 512)) return false;
-
-    return true;
-  });
-  auto swiz_mult3 = UnitTest("swizzle assignment operator *= (3)", +[]() {
-    ivec4 v = ivec4(64, 128, 256, 512);
-
-    v.w *= 2;
-    v.zz *= 2;
-    v.yyy *= 2;
-    v.xxxx *= 2;
-
-    if (v != 1024) return false;
-
-    return true;
-  });
-  auto swiz_mult4 = UnitTest("swizzle assignment operator *= (4)", +[]() {
-    ivec4 v = ivec4(2);
-
-    v.x *= v.x;
-    v.yx *= v.xy;
-    v.zyx *= v.xyz;
-    v.wzyx *= v.xyzw;
-
-    if (v != ivec4(32, 1024, 1024, 32)) return false;
-
-    return true;
-  });
-
-  auto swiz_div1 = UnitTest("swizzle assignment operator /= (1)", +[]() {
-    ivec4 v = ivec4(16);
-
-    v.x /= 2;
-    v.xy /= 2;
-    v.zyx /= 2;
-    v.xwzy /= 2;
-
-    if (v != ivec4(1, 2, 4, 8)) return false;
-
-    return true;
-  });
-  auto swiz_div2 = UnitTest("swizzle assignment operator /= (2)", +[]() {
-    ivec4 v = ivec4(64, 128, 256, 512);
-
-    v.xy /= ivec2(4, 2);
-    v.yzw /= ivec3(2, 4, 8);
-    v.xyzw /= ivec4(1, 2, 4, 4);
-
-    if (v != 16) return false;
-
-    return true;
-  });
-  auto swiz_div3 = UnitTest("swizzle assignment operator /= (3)", +[]() {
-    ivec4 v = ivec4(1024);
-
-    v.w /= 2;
-    v.zz /= 2;
-    v.yyy /= 2;
-    v.xxxx /= 2;
-
-    if (v != ivec4(64, 128, 256, 512)) return false;
-
-    return true;
-  });
-  auto swiz_div4 = UnitTest("swizzle assignment operator /= (4)", +[]() {
-    ivec4 v = ivec4(1024);
-    ivec4 u = ivec4(2, 4, 8, 16);
-
-    v.xyzw /= u.wzyx;
-    v.yzw /= u.zyx;
-    v.zw /= u.yx;
-    v.w /= u.x;
-
-    if (v != ivec4(64, 16, 16, 64)) return false;
-
-    return true;
-  });
-
-  auto swiz_asig1 = UnitTest("swizzle assignment operator = (1)", +[]() {
-    ivec4 v = ivec4(16);
-
-    v.x = 1;
-    if (v != ivec4(1, 16, 16, 16)) return false;
-
-    v.xy = 2;
-    if (v != ivec4(2, 2, 16, 16)) return false;
-
-    v.zyx = 3;
-    if (v != ivec4(3, 3, 3, 16)) return false;
-
-    v.xwzy = 4;
-    if (v != 4) return false;
-
-    return true;
-  });
-  auto swiz_asig2 = UnitTest("swizzle assignment operator = (2)", +[]() {
-    ivec4 v = ivec4(0);
-
-    v.xy = ivec2(1, 2);
-    if (v != ivec4(1, 2, 0, 0)) return false;
-
-    v.yzw = ivec3(1);
-    if (v != 1) return false;
-
-    v.xyzw = ivec4(1, 2, 3, 4);
-    if (v != ivec4(1, 2, 3, 4)) return false;
-
-    return true;
-  });
-  auto swiz_asig3 = UnitTest("swizzle assignment operator = (3)", +[]() {
-    ivec4 v = ivec4(1024);
-
-    v.w = 2;
-    v.zz = 2;
-    v.yyy = 2;
-    v.xxxx = 2;
-
-    if (v != 2) return false;
-
-    return true;
-  });
-  auto swiz_asig4 = UnitTest("swizzle assignment operator = (4)", +[]() {
-    ivec4 v = ivec4(1, 2, 3, 4);
-    ivec4 u = ivec4(5, 6, 7, 8);
-
-    v.x = u.x;
-    if (v != ivec4(5, 2, 3, 4)) return false;
-
-    v.xy = u.xy;
-    if (v != ivec4(5, 6, 3, 4)) return false;
-
-    v.xyz = u.xyz;
-    if (v != ivec4(5, 6, 7, 4)) return false;
-
-    v.xyzw = u.wzyx;
-    if (v != ivec4(8, 7, 6, 5)) return false;
-
-    return true;
-  });
-  auto swiz_asig5 = UnitTest("swizzle assignment operator = (5)", +[]() {
-    vec<5> v = vec<5>(1, 2, 3, 4, 5);
-
-    v.x = 0;
-    if (v != vec<5>(0, 2, 3, 4, 5)) return false;
-
-    v.xy = 1;
-    if (v != vec<5>(1, 1, 3, 4, 5)) return false;
-
-    v.xyz = 2;
-    if (v != vec<5>(2, 2, 2, 4, 5)) return false;
-
-    v.xyzw = 3;
-    if (v != vec<5>(3, 3, 3, 3, 5)) return false;
-
-    return true;
-  });
+using namespace mmm;
+
+TEST_CASE("swizzle assignment operator +=", "[vec][asig]") {
+
+  vec2 v2 = vec2(1, 2);
+  vec3 v3 = vec3(1, 2, 3);
+  vec4 v4 = vec4(1, 2, 3, 4);
+
+  vec2 u2 = vec2(1, 2);
+  vec3 u3 = vec3(1, 2, 3);
+  vec4 u4 = vec4(1, 2, 3, 4);
+
+  SECTION("swizzle1") {
+    v2.x += 1;
+    v3.x += 1;
+    v4.x += u4.x;
+    REQUIRE(v2 == vec2(2, 2));
+    REQUIRE(v3 == vec3(2, 2, 3));
+    REQUIRE(v4 == vec4(2, 2, 3, 4));
+  }
+
+  SECTION("swizzle2") {
+    v2.xy += 1;
+    v3.xy += u2;
+    v4.xy += u4.xy;
+    REQUIRE(v2 == vec2(2, 3));
+    REQUIRE(v3 == vec3(2, 4, 3));
+    REQUIRE(v4 == vec4(2, 4, 3, 4));
+  }
+
+  SECTION("swizzle3") {
+    v2.xyy += 1;
+    v3.xyz += u3;
+    v4.xyz += u4.xyz;
+    REQUIRE(v2 == vec2(2, 4));
+    REQUIRE(v3 == vec3(2, 4, 6));
+    REQUIRE(v4 == vec4(2, 4, 6, 4));
+  }
+
+  SECTION("swizzle4") {
+    v2.xyyy += 1;
+    v3.xyzz += u4;
+    v4.xyzw += u4.xyzw;
+    REQUIRE(v2 == vec2(2, 5));
+    REQUIRE(v3 == vec3(2, 4, 10));
+    REQUIRE(v4 == vec4(2, 4, 6, 8));
+  }
+}
+
+TEST_CASE("swizzle assignment operator -=", "[vec][asig]") {
+
+  vec2 v2 = vec2(1, 2);
+  vec3 v3 = vec3(1, 2, 3);
+  vec4 v4 = vec4(1, 2, 3, 4);
+
+  vec2 u2 = vec2(1, 2);
+  vec3 u3 = vec3(1, 2, 3);
+  vec4 u4 = vec4(1, 2, 3, 4);
+
+  SECTION("swizzle1") {
+    v2.x -= 1;
+    v3.x -= 1;
+    v4.x -= u4.x;
+    REQUIRE(v2 == vec2(0, 2));
+    REQUIRE(v3 == vec3(0, 2, 3));
+    REQUIRE(v4 == vec4(0, 2, 3, 4));
+  }
+
+  SECTION("swizzle2") {
+    v2.xy -= 1;
+    v3.xy -= u2;
+    v4.xy -= u4.xy;
+    REQUIRE(v2 == vec2(0, 1));
+    REQUIRE(v3 == vec3(0, 0, 3));
+    REQUIRE(v4 == vec4(0, 0, 3, 4));
+  }
+
+  SECTION("swizzle3") {
+    v2.xyy -= 1;
+    v3.xyz -= u3;
+    v4.xyz -= u4.xyz;
+    REQUIRE(v2 == vec2(0, 0));
+    REQUIRE(v3 == vec3(0, 0, 0));
+    REQUIRE(v4 == vec4(0, 0, 0, 4));
+  }
+
+  SECTION("swizzle4") {
+    v2.xyyy -= 1;
+    v3.xyzz -= u4;
+    v4.xyzw -= u4.xyzw;
+    REQUIRE(v2 == vec2(0, -1));
+    REQUIRE(v3 == vec3(0, 0, -4));
+    REQUIRE(v4 == vec4(0, 0, 0, 0));
+  }
+}
+
+TEST_CASE("swizzle assignment operator *=", "[vec][asig]") {
+
+  vec2 v2 = vec2(1, 2);
+  vec3 v3 = vec3(1, 2, 3);
+  vec4 v4 = vec4(1, 2, 3, 4);
+
+  vec2 u2 = vec2(1, 2);
+  vec3 u3 = vec3(1, 2, 3);
+  vec4 u4 = vec4(1, 2, 3, 4);
+
+  SECTION("swizzle1") {
+    v2.x *= 2;
+    v3.x *= 2;
+    v4.x *= u4.x;
+    REQUIRE(v2 == vec2(2, 2));
+    REQUIRE(v3 == vec3(2, 2, 3));
+    REQUIRE(v4 == vec4(1, 2, 3, 4));
+  }
+
+  SECTION("swizzle2") {
+    v2.xy *= 2;
+    v3.xy *= u2;
+    v4.xy *= u4.xy;
+    REQUIRE(v2 == vec2(2, 4));
+    REQUIRE(v3 == vec3(1, 4, 3));
+    REQUIRE(v4 == vec4(1, 4, 3, 4));
+  }
+
+  SECTION("swizzle3") {
+    v2.xyy *= 2;
+    v3.xyz *= u3;
+    v4.xyz *= u4.xyz;
+    REQUIRE(v2 == vec2(2, 8));
+    REQUIRE(v3 == vec3(1, 4, 9));
+    REQUIRE(v4 == vec4(1, 4, 9, 4));
+  }
+
+  SECTION("swizzle4") {
+    v2.xyyy *= 2;
+    v3.xyzz *= u4;
+    v4.xyzw *= u4.xyzw;
+    REQUIRE(v2 == vec2(2, 16));
+    REQUIRE(v3 == vec3(1, 4, 36));
+    REQUIRE(v4 == vec4(1, 4, 9, 16));
+  }
+}
+
+TEST_CASE("swizzle assignment operator /=", "[vec][asig]") {
+
+  vec2 v2 = vec2(4, 8);
+  vec3 v3 = vec3(4, 8, 16);
+  vec4 v4 = vec4(4, 8, 16, 32);
+
+  vec2 u2 = vec2(1, 2);
+  vec3 u3 = vec3(1, 2, 4);
+  vec4 u4 = vec4(1, 2, 4, 8);
+
+  SECTION("swizzle1") {
+    v2.x /= 2;
+    v3.x /= 2;
+    v4.x /= u4.x;
+    REQUIRE(v2 == vec2(2, 8));
+    REQUIRE(v3 == vec3(2, 8, 16));
+    REQUIRE(v4 == vec4(4, 8, 16, 32));
+  }
+
+  SECTION("swizzle2") {
+    v2.xy /= 2;
+    v3.xy /= u2;
+    v4.xy /= u4.xy;
+    REQUIRE(v2 == vec2(2, 4));
+    REQUIRE(v3 == vec3(4, 4, 16));
+    REQUIRE(v4 == vec4(4, 4, 16, 32));
+  }
+
+  SECTION("swizzle3") {
+    v2.xyy /= 2;
+    v3.xyz /= u3;
+    v4.xyz /= u4.xyz;
+    REQUIRE(v2 == vec2(2, 2));
+    REQUIRE(v3 == vec3(4, 4, 4));
+    REQUIRE(v4 == vec4(4, 4, 4, 32));
+  }
+
+  SECTION("swizzle4") {
+    v2.xyyy /= 2;
+    v3.xyzz /= u4;
+    v4.xyzw /= u4.xyzw;
+    REQUIRE(v2 == vec2(2, 1));
+    REQUIRE(v3 == vec3(4, 4, 0.5));
+    REQUIRE(v4 == vec4(4, 4, 4, 4));
+  }
+}
+
+TEST_CASE("swizzle assignment operator =", "[vec][asig]") {
+
+  vec2 v2 = vec2(1, 2);
+  vec3 v3 = vec3(1, 2, 3);
+  vec4 v4 = vec4(1, 2, 3, 4);
+
+  vec2 u2 = vec2(5, 6);
+  vec3 u3 = vec3(5, 6, 7);
+  vec4 u4 = vec4(5, 6, 7, 8);
+
+  SECTION("swizzle1") {
+    v2.x = 0;
+    v3.x = 0;
+    v4.x = u4.x;
+    REQUIRE(v2 == vec2(0, 2));
+    REQUIRE(v3 == vec3(0, 2, 3));
+    REQUIRE(v4 == vec4(5, 2, 3, 4));
+  }
+
+  SECTION("swizzle2") {
+    v2.xy = 0;
+    v3.xy = u2;
+    v4.xy = u4.xy;
+    REQUIRE(v2 == vec2(0, 0));
+    REQUIRE(v3 == vec3(5, 6, 3));
+    REQUIRE(v4 == vec4(5, 6, 3, 4));
+  }
+
+  SECTION("swizzle3") {
+    v2.xyy = 0;
+    v3.xyz = u3;
+    v4.xyz = u4.xyz;
+    REQUIRE(v2 == vec2(0, 0));
+    REQUIRE(v3 == vec3(5, 6, 7));
+    REQUIRE(v4 == vec4(5, 6, 7, 4));
+  }
+
+  SECTION("swizzle4") {
+    v2.xyyy = 0;
+    v3.xyzz = u4;
+    v4.xyzw = u4.xyzw;
+    REQUIRE(v2 == vec2(0, 0));
+    REQUIRE(v3 == vec3(5, 6, 8));
+    REQUIRE(v4 == vec4(5, 6, 7, 8));
+  }
 }
--- test/swizzle/opr/enum.cpp
+++ test/swizzle/opr/enum.cpp
@@ -1,76 +1,186 @@
-#include <unittest.hpp>
+#include <catch.hpp>
 #include <mmm.hpp>
 
-namespace {
-  using namespace mmm;
+using namespace mmm;
 
-  auto swiz_succ1 = UnitTest("swizzle assignment operator ++ (1)", +[]() {
-    ivec4 v = ivec4(1, 2, 3, 4);
+TEST_CASE("swizzle enumeration operator ++", "[vec][enum]") {
 
-    if (++v.x != 2) return false;
-    if (v != ivec4(2, 2, 3, 4)) return false;
+  ivec2 v2 = ivec2(1, 2);
+  ivec3 v3 = ivec3(1, 2, 3);
+  ivec4 v4 = ivec4(1, 2, 3, 4);
 
-    if (++v.xy != ivec2(3, 3)) return false;
-    if (v != ivec4(3, 3, 3, 4)) return false;
+  SECTION("swizzle1 post") {
+    REQUIRE(++v2.x == 2);
+    REQUIRE(v2 == ivec2(2, 2));
 
-    if (++v.xyz != ivec3(4, 4, 4)) return false;
-    if (v != ivec4(4, 4, 4, 4)) return false;
+    REQUIRE(++v3.x == 2);
+    REQUIRE(v3 == ivec3(2, 2, 3));
 
-    if (++v.xyzw != ivec4(5, 5, 5, 5)) return false;
-    if (v != ivec4(5, 5, 5, 5)) return false;
+    REQUIRE(++v4.x == 2);
+    REQUIRE(v4 == ivec4(2, 2, 3, 4));
+  }
+  SECTION("swizzle1 post") {
+    REQUIRE(v2.x++ == 1);
+    REQUIRE(v2 == ivec2(2, 2));
 
-    return true;
-  });
-  auto swiz_succ2 = UnitTest("swizzle assignment operator ++ (2)", +[]() {
-    ivec4 v = ivec4(1, 2, 3, 4);
+    REQUIRE(v3.x++ == 1);
+    REQUIRE(v3 == ivec3(2, 2, 3));
 
-    if (v.x++ != 1) return false;
-    if (v != ivec4(2, 2, 3, 4)) return false;
+    REQUIRE(v4.x++ == 1);
+    REQUIRE(v4 == ivec4(2, 2, 3, 4));
+  }
 
-    if (v.xy++ != ivec2(2, 2)) return false;
-    if (v != ivec4(3, 3, 3, 4)) return false;
+  SECTION("swizzle2 pre") {
+    REQUIRE(++v2.xy == ivec2(2, 3));
+    REQUIRE(v2 == ivec2(2, 3));
 
-    if (v.xyz++ != ivec3(3, 3, 3)) return false;
-    if (v != ivec4(4, 4, 4, 4)) return false;
+    REQUIRE(++v3.xy == ivec2(2, 3));
+    REQUIRE(v3 == ivec3(2, 3, 3));
 
-    if (v.xyzw++ != ivec4(4, 4, 4, 4)) return false;
-    if (v != ivec4(5, 5, 5, 5)) return false;
+    REQUIRE(++v4.xy == ivec2(2, 3));
+    REQUIRE(v4 == ivec4(2, 3, 3, 4));
+  }
+  SECTION("swizzle2 post") {
+    REQUIRE(v2.xy++ == ivec2(1, 2));
+    REQUIRE(v2 == ivec2(2, 3));
 
-    return true;
-  });
+    REQUIRE(v3.xy++ == ivec2(1, 2));
+    REQUIRE(v3 == ivec3(2, 3, 3));
 
-  auto swiz_prev1 = UnitTest("swizzle assignment operator -- (1)", +[]() {
-    ivec4 v = ivec4(5, 4, 3, 2);
+    REQUIRE(v4.xy++ == ivec2(1, 2));
+    REQUIRE(v4 == ivec4(2, 3, 3, 4));
+  }
 
-    if (--v.x != 4) return false;
-    if (v != ivec4(4, 4, 3, 2)) return false;
+  SECTION("swizzle3 pre") {
+    REQUIRE(++v2.xyy == ivec3(2, 4, 4));
+    REQUIRE(v2 == ivec2(2, 4));
 
-    if (--v.xy != ivec2(3, 3)) return false;
-    if (v != ivec4(3, 3, 3, 2)) return false;
+    REQUIRE(++v3.xyz == ivec3(2, 3, 4));
+    REQUIRE(v3 == ivec3(2, 3, 4));
 
-    if (--v.xyz != ivec3(2, 2, 2)) return false;
-    if (v != ivec4(2, 2, 2, 2)) return false;
+    REQUIRE(++v4.xyz == ivec3(2, 3, 4));
+    REQUIRE(v4 == ivec4(2, 3, 4, 4));
+  }
+  SECTION("swizzle3 post") {
+    REQUIRE(v2.xyy++ == ivec3(1, 2, 2));
+    REQUIRE(v2 == ivec2(2, 4));
 
-    if (--v.xyzw != ivec4(1, 1, 1, 1)) return false;
-    if (v != ivec4(1, 1, 1, 1)) return false;
+    REQUIRE(v3.xyz++ == ivec3(1, 2, 3));
+    REQUIRE(v3 == ivec3(2, 3, 4));
 
-    return true;
-  });
-  auto swiz_prev2 = UnitTest("swizzle assignment operator -- (2)", +[]() {
-    ivec4 v = ivec4(5, 4, 3, 2);
+    REQUIRE(v4.xyz++ == ivec3(1, 2, 3));
+    REQUIRE(v4 == ivec4(2, 3, 4, 4));
+  }
 
-    if (v.x-- != 5) return false;
-    if (v != ivec4(4, 4, 3, 2)) return false;
+  SECTION("swizzle4 pre") {
+    REQUIRE(++v2.xyyy == ivec4(2, 5, 5, 5));
+    REQUIRE(v2 == ivec2(2, 5));
 
-    if (v.xy-- != ivec2(4, 4)) return false;
-    if (v != ivec4(3, 3, 3, 2)) return false;
+    REQUIRE(++v3.xyzz == ivec4(2, 3, 5, 5));
+    REQUIRE(v3 == ivec3(2, 3, 5));
 
-    if (v.xyz-- != ivec3(3, 3, 3)) return false;
-    if (v != ivec4(2, 2, 2, 2)) return false;
+    REQUIRE(++v4.xyzw == ivec4(2, 3, 4, 5));
+    REQUIRE(v4 == ivec4(2, 3, 4, 5));
+  }
+  SECTION("swizzle4 post") {
+    REQUIRE(v2.xyyy++ == ivec4(1, 2, 2, 2));
+    REQUIRE(v2 == ivec2(2, 5));
 
-    if (v.xyzw-- != ivec4(2, 2, 2, 2)) return false;
-    if (v != ivec4(1, 1, 1, 1)) return false;
+    REQUIRE(v3.xyzz++ == ivec4(1, 2, 3, 3));
+    REQUIRE(v3 == ivec3(2, 3, 5));
 
-    return true;
-  });
+    REQUIRE(v4.xyzw++ == ivec4(1, 2, 3, 4));
+    REQUIRE(v4 == ivec4(2, 3, 4, 5));
+  }
+}
+
+TEST_CASE("swizzle enumeration operator --", "[vec][enum]") {
+
+  ivec2 v2 = ivec2(1, 2);
+  ivec3 v3 = ivec3(1, 2, 3);
+  ivec4 v4 = ivec4(1, 2, 3, 4);
+
+  SECTION("swizzle1 post") {
+    REQUIRE(--v2.x == 0);
+    REQUIRE(v2 == ivec2(0, 2));
+
+    REQUIRE(--v3.x == 0);
+    REQUIRE(v3 == ivec3(0, 2, 3));
+
+    REQUIRE(--v4.x == 0);
+    REQUIRE(v4 == ivec4(0, 2, 3, 4));
+  }
+  SECTION("swizzle1 post") {
+    REQUIRE(v2.x-- == 1);
+    REQUIRE(v2 == ivec2(0, 2));
+
+    REQUIRE(v3.x-- == 1);
+    REQUIRE(v3 == ivec3(0, 2, 3));
+
+    REQUIRE(v4.x-- == 1);
+    REQUIRE(v4 == ivec4(0, 2, 3, 4));
+  }
+
+  SECTION("swizzle2 pre") {
+    REQUIRE(--v2.xy == ivec2(0, 1));
+    REQUIRE(v2 == ivec2(0, 1));
+
+    REQUIRE(--v3.xy == ivec2(0, 1));
+    REQUIRE(v3 == ivec3(0, 1, 3));
+
+    REQUIRE(--v4.xy == ivec2(0, 1));
+    REQUIRE(v4 == ivec4(0, 1, 3, 4));
+  }
+  SECTION("swizzle2 post") {
+    REQUIRE(v2.xy-- == ivec2(1, 2));
+    REQUIRE(v2 == ivec2(0, 1));
+
+    REQUIRE(v3.xy-- == ivec2(1, 2));
+    REQUIRE(v3 == ivec3(0, 1, 3));
+
+    REQUIRE(v4.xy-- == ivec2(1, 2));
+    REQUIRE(v4 == ivec4(0, 1, 3, 4));
+  }
+
+  SECTION("swizzle3 pre") {
+    REQUIRE(--v2.xyy == ivec3(0, 0, 0));
+    REQUIRE(v2 == ivec2(0, 0));
+
+    REQUIRE(--v3.xyz == ivec3(0, 1, 2));
+    REQUIRE(v3 == ivec3(0, 1, 2));
+
+    REQUIRE(--v4.xyz == ivec3(0, 1, 2));
+    REQUIRE(v4 == ivec4(0, 1, 2, 4));
+  }
+  SECTION("swizzle3 post") {
+    REQUIRE(v2.xyy-- == ivec3(1, 2, 2));
+    REQUIRE(v2 == ivec2(0, 0));
+
+    REQUIRE(v3.xyz-- == ivec3(1, 2, 3));
+    REQUIRE(v3 == ivec3(0, 1, 2));
+
+    REQUIRE(v4.xyz-- == ivec3(1, 2, 3));
+    REQUIRE(v4 == ivec4(0, 1, 2, 4));
+  }
+
+  SECTION("swizzle4 pre") {
+    REQUIRE(--v2.xyyy == ivec4(0, -1, -1, -1));
+    REQUIRE(v2 == ivec2(0, -1));
+
+    REQUIRE(--v3.xyzz == ivec4(0, 1, 1, 1));
+    REQUIRE(v3 == ivec3(0, 1, 1));
+
+    REQUIRE(--v4.xyzw == ivec4(0, 1, 2, 3));
+    REQUIRE(v4 == ivec4(0, 1, 2, 3));
+  }
+  SECTION("swizzle4 post") {
+    REQUIRE(v2.xyyy-- == ivec4(1, 2, 2, 2));
+    REQUIRE(v2 == ivec2(0, -1));
+
+    REQUIRE(v3.xyzz-- == ivec4(1, 2, 3, 3));
+    REQUIRE(v3 == ivec3(0, 1, 1));
+
+    REQUIRE(v4.xyzw-- == ivec4(1, 2, 3, 4));
+    REQUIRE(v4 == ivec4(0, 1, 2, 3));
+  }
 }
