cd1c8ed01c7f97ad525f07b2c9655c3651d8d099
Thhethssmuz <thhethssmuz@gmail.com>
2017-02-08 19:03:40 +0100

    build single test

--- run_tests.sh
+++ run_tests.sh
@@ -3,6 +3,23 @@ set -e
 
 BUILD_ARGS=( -DMMM_BUILD_UNIT_TESTS=1 -DUSE_LIBDW=0 )
 
+# if any arguments are given then only build the first test matching all the
+# given patterns
+if [ $# -gt 0 ]; then
+
+  SRC=$(find test/ -type f -name "*.cpp" | sed 's/test\///')
+  while [[ $# -gt 0 ]]; do
+    SRC=$(echo "$SRC" | grep "$1") || (echo "no test matched"; exit 1)
+    shift
+  done
+
+  SRC=$(echo "$SRC" | head -n1)
+
+  BUILD_ARGS+=( -DMMM_BUILD_UNIT_TESTS_SINGLE="$SRC" )
+else
+  BUILD_ARGS+=( -DMMM_BUILD_UNIT_TESTS_SINGLE=0 )
+fi
+
 # create build directory
 mkdir -p build
 cd build
