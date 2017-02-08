6279a1e261e76f768d2158767141fcbf06c7e1a7
Thhethssmuz <thhethssmuz@gmail.com>
2017-02-08 19:01:33 +0100

    use ninja if available

--- run_tests.sh
+++ run_tests.sh
@@ -1,8 +1,19 @@
-#!/bin/sh
+#!/usr/bin/env bash
 set -e
 
+BUILD_ARGS=( -DMMM_BUILD_UNIT_TESTS=1 -DUSE_LIBDW=0 )
+
+# create build directory
 mkdir -p build
 cd build
-cmake .. -DMMM_BUILD_UNIT_TESTS=1 -DUSE_LIBDW=0
-make -j4 mmm_tests #VERBOSE=1
-ctest -R mmm_tests --output-on-failure
+
+# prefer ninja over make if available
+if hash ninja 2>/dev/null; then
+  cmake -GNinja .. "${BUILD_ARGS[@]}"
+  ninja
+else
+  cmake .. "${BUILD_ARGS[@]}"
+  make -j4 mmm_tests #VERBOSE=1
+fi
+
+ctest -R mmm_tests --verbose # --output-on-failure
