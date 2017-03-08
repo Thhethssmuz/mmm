66924aa1ce43f108dee3920e173ddd90cb7c24fb
Thhethssmuz <thhethssmuz@gmail.com>
2017-03-09 00:39:52 +0100

    add tests for missing matrix affine functions

--- test/mat/func/affine.cpp
+++ test/mat/func/affine.cpp
@@ -116,3 +116,147 @@ TEST_CASE("affine matrix - scale", "[mat][affine]") {
 
   REQUIRE(s == mat4(2,0,0,0, 0,3,0,0, 0,0,4,0, 0,0,0,1));
 }
+
+
+TEST_CASE("affine matrix - lookAt", "[mat][affine]") {
+
+  vec3 e = vec3(2, 0, 0);
+  vec3 c = vec3(0, 0, 0);
+  vec3 u = vec3(0, 1, 0);
+
+  SECTION("default") {
+    mat4 m = lookAt(e, c, u);
+    REQUIRE(m == mat4(0,0,1,0, 0,1,0,0, -1,0,0,2, 0,0,0,1));
+  }
+
+  SECTION("left-handed") {
+    mat4 m = lookAt_l(e, c, u);
+    REQUIRE(m == mat4(0,0,1,0, 0,1,0,0, -1,0,0,2, 0,0,0,1));
+  }
+
+  SECTION("right-handed") {
+    mat4 m = lookAt_r(e, c, u);
+    REQUIRE(m == mat4(0,0,-1,0, 0,1,0,0, 1,0,0,-2, 0,0,0,1));
+  }
+}
+
+TEST_CASE("affine matrix - perspective", "[mat][affine]") {
+
+  SECTION("default") {
+    mat4 m = perspective(45.f, 1.f, 1.f, 100.f);
+
+    REQUIRE(m[0][0] == Approx(2.41421));
+    REQUIRE(m[0][1] == Approx(0));
+    REQUIRE(m[0][2] == Approx(0));
+    REQUIRE(m[0][3] == Approx(0));
+
+    REQUIRE(m[1][0] == Approx(0));
+    REQUIRE(m[1][1] == Approx(2.41421));
+    REQUIRE(m[1][2] == Approx(0));
+    REQUIRE(m[1][3] == Approx(0));
+
+    REQUIRE(m[2][0] == Approx(0));
+    REQUIRE(m[2][1] == Approx(0));
+    REQUIRE(m[2][2] == Approx(1.0202));
+    REQUIRE(m[2][3] == Approx(-2.0202));
+
+    REQUIRE(m[3][0] == Approx(0));
+    REQUIRE(m[3][1] == Approx(0));
+    REQUIRE(m[3][2] == Approx(1));
+    REQUIRE(m[3][3] == Approx(0));
+  }
+
+  SECTION("left-handed") {
+    mat4 m = perspective_l(45.f, 1.f, 1.f, 100.f);
+
+    REQUIRE(m[0][0] == Approx(2.41421));
+    REQUIRE(m[0][1] == Approx(0));
+    REQUIRE(m[0][2] == Approx(0));
+    REQUIRE(m[0][3] == Approx(0));
+
+    REQUIRE(m[1][0] == Approx(0));
+    REQUIRE(m[1][1] == Approx(2.41421));
+    REQUIRE(m[1][2] == Approx(0));
+    REQUIRE(m[1][3] == Approx(0));
+
+    REQUIRE(m[2][0] == Approx(0));
+    REQUIRE(m[2][1] == Approx(0));
+    REQUIRE(m[2][2] == Approx(1.0202));
+    REQUIRE(m[2][3] == Approx(-2.0202));
+
+    REQUIRE(m[3][0] == Approx(0));
+    REQUIRE(m[3][1] == Approx(0));
+    REQUIRE(m[3][2] == Approx(1));
+    REQUIRE(m[3][3] == Approx(0));
+  }
+
+  SECTION("right-handed") {
+    mat4 m = perspective_r(45.f, 1.f, 1.f, 100.f);
+
+    REQUIRE(m[0][0] == Approx(2.41421));
+    REQUIRE(m[0][1] == Approx(0));
+    REQUIRE(m[0][2] == Approx(0));
+    REQUIRE(m[0][3] == Approx(0));
+
+    REQUIRE(m[1][0] == Approx(0));
+    REQUIRE(m[1][1] == Approx(2.41421));
+    REQUIRE(m[1][2] == Approx(0));
+    REQUIRE(m[1][3] == Approx(0));
+
+    REQUIRE(m[2][0] == Approx(0));
+    REQUIRE(m[2][1] == Approx(0));
+    REQUIRE(m[2][2] == Approx(-1.0202));
+    REQUIRE(m[2][3] == Approx(-2.0202));
+
+    REQUIRE(m[3][0] == Approx(0));
+    REQUIRE(m[3][1] == Approx(0));
+    REQUIRE(m[3][2] == Approx(-1));
+    REQUIRE(m[3][3] == Approx(0));
+  }
+}
+
+TEST_CASE("affine matrix - ortho", "[mat][affine]") {
+
+  SECTION("default") {
+    mat4 m = ortho(-1.f, 1.f, -1.f, 1.f, -1.f, 1.f);
+    mat4 n = ortho(0.f, 200.f, 100.f, 0.f, 0.f, 1.f);
+
+    REQUIRE(m == mat4(1,0,0,0, 0,1,0,0, 0,0,1,0, 0,0,0,1));
+    REQUIRE(n == mat4(0.01,0,0,-1, 0,-0.02,0,1, 0,0,2,-1, 0,0,0,1));
+  }
+
+  SECTION("left-handed") {
+    mat4 m = ortho_l(-1.f, 1.f, -1.f, 1.f, -1.f, 1.f);
+    mat4 n = ortho_l(0.f, 200.f, 100.f, 0.f, 0.f, 1.f);
+    REQUIRE(m == mat4(1,0,0,0, 0,1,0,0, 0,0,1,0, 0,0,0,1));
+    REQUIRE(n == mat4(0.01,0,0,-1, 0,-0.02,0,1, 0,0,2,-1, 0,0,0,1));
+  }
+
+  SECTION("right-handed") {
+    mat4 m = ortho_r(-1.f, 1.f, -1.f, 1.f, -1.f, 1.f);
+    mat4 n = ortho_r(0.f, 200.f, 100.f, 0.f, 0.f, 1.f);
+    REQUIRE(m == mat4(1,0,0,0, 0,1,0,0, 0,0,-1,0, 0,0,0,1));
+    REQUIRE(n == mat4(0.01,0,0,-1, 0,-0.02,0,1, 0,0,-2,-1, 0,0,0,1));
+  }
+}
+
+TEST_CASE("affine matrix - unProject", "[mat][affine]") {
+
+  vec3 org = vec3(0, 0, 0);
+
+  vec3 eye      = normalize(vec3(1));
+  mat4 model    = lookAt(eye, vec3(0, 0, 0), vec3(0, 1, 0));
+  mat4 proj     = ortho(-1.f, 1.f, -1.f, 1.f, -1.f, 1.f);
+  vec4 viewport = vec4(0, 0, 1024.f, 768.f);
+
+  vec4 pos      = proj * model * vec4(org, 1);
+  vec3 win      = vec3((pos.x + 1) * (viewport.z / 2) + viewport.x,
+                       (pos.y + 1) * (viewport.w / 2) + viewport.y,
+                       1);
+
+  vec3 traced = unProject(win, model, proj, viewport);
+
+  REQUIRE(org.x == Approx(traced.x));
+  REQUIRE(org.y == Approx(traced.y));
+  REQUIRE(org.z == Approx(traced.z));
+}
